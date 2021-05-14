c89b615a55204af52ab0152262d1d78c74af848a
Константин Евлампиев <kevlamiev@gmail.com>
2021-05-15 02:43:19 +0700

    not significant changes for vehicleTypes 2

--- vehicles.local/app/Http/Requests/VehicleTypeRequest.php
+++ vehicles.local/app/Http/Requests/VehicleTypeRequest.php
@@ -24,7 +24,7 @@ class VehicleTypeRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string'
+            'name' => 'string|between: 5,100'
         ];
     }
 
